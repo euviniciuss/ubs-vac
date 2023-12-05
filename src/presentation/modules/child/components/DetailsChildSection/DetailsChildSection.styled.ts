1066114ce31b0d34f830b85f428eb83955ef1c62
euviniciuss <dev.mvsc@gmail.com>
2023-12-05 03:44:38 -0300

    feat: criando card de infos da crianca

--- src/presentation/modules/child/components/DetailsChildSection/DetailsChildSection.styled.ts
+++ src/presentation/modules/child/components/DetailsChildSection/DetailsChildSection.styled.ts
@@ -10,13 +10,15 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 5.2rem;
+  padding-bottom: 4rem;
 `
 
 export const ProfileContainer = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  gap: 2rem;
 `
 
 export const Profile = styled.div`
@@ -41,4 +43,57 @@ export const Info = styled.div`
     font-weight: 600;
     color: ${theme.colors.blue800};
   }
+`
+export const LinkContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 1rem;
+
+  p {
+    font-size: 1.6rem;
+    font-weight: 600;
+    color: ${theme.colors.blue800};
+    max-width: 14.6rem;
+  }
+`
+export const InfoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+export const InfoCard = styled.div`
+  width: 100%;
+  border: 1px solid ${theme.colors.blue700};
+`
+
+export const Header = styled.div`
+  background-color: ${theme.colors.blue700};
+  color: ${theme.colors.white};
+  padding: 1.4rem 2rem;
+  font-size: 1.15rem;
+  font-weight: 600;
+`
+
+export const Infos = styled.div`
+  padding: 1.4rem 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+`
+
+export const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Name = styled.p`
+  font-size: 1.6rem;
+  color: ${theme.colors.black500};
+  opacity: 0.7;
+`
+
+export const Value = styled.p`
+  font-size: 1.6rem;
+  color: ${theme.colors.black500};
 `
--- src/presentation/modules/child/components/DetailsChildSection/DetailsChildSection.tsx
+++ src/presentation/modules/child/components/DetailsChildSection/DetailsChildSection.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { PiCaretRightBold } from 'react-icons/pi'
 
 import Boy from '@/public/boy.svg'
 
@@ -17,10 +18,91 @@ export function DetailsChildSection() {
           <h1>João Pedro Costa</h1>
 
           <Link href="/">
-            <p> Marcos de desenvolvimento</p>
+            <S.LinkContainer>
+              <p>Marcos de desenvolvimento</p>
+              <PiCaretRightBold size={17} color="1040AB" />
+            </S.LinkContainer>
           </Link>
         </S.Info>
       </S.ProfileContainer>
+
+      <S.InfoContainer>
+        <S.InfoCard>
+          <S.Header>
+            <h1>Informações Pessoais</h1>
+          </S.Header>
+
+          <S.Infos>
+            <S.Field>
+              <S.Name>RG</S.Name>
+              <S.Value>000000000000-0</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>CPF</S.Name>
+              <S.Value>000.000.000-00</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>Data de Nascimento</S.Name>
+              <S.Value>01/10/2019</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>Sexo</S.Name>
+              <S.Value>Masculino</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>Tipo sanguíneo</S.Name>
+              <S.Value>O-</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>Alergias</S.Name>
+              <S.Value>Não possui</S.Value>
+            </S.Field>
+          </S.Infos>
+        </S.InfoCard>
+
+        <S.InfoCard>
+          <S.Header>
+            <h1>Endereço</h1>
+          </S.Header>
+
+          <S.Infos>
+            <S.Field>
+              <S.Name>Cidade</S.Name>
+              <S.Value>São Luís</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>Estado</S.Name>
+              <S.Value>Maranhão</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>Logradouro</S.Name>
+              <S.Value>Avenida Coronel Colares Moreira</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>Bairro</S.Name>
+              <S.Value>Jardim Renasceça</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>CEP</S.Name>
+              <S.Value>000000-000</S.Value>
+            </S.Field>
+
+            <S.Field>
+              <S.Name>Número</S.Name>
+              <S.Value>15</S.Value>
+            </S.Field>
+          </S.Infos>
+        </S.InfoCard>
+      </S.InfoContainer>
     </S.Container>
   )
 }
